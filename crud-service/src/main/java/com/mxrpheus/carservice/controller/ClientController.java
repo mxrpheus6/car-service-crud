0a2e467a886c16acd4f2163c0ef9597b987700c1
mxrpheus6 <pkazachenkoo@gmail.com>
2025-03-20 01:01:24 +0300

    Changed put to patch endpoint

--- crud-service/src/main/java/com/mxrpheus/carservice/controller/ClientController.java
+++ crud-service/src/main/java/com/mxrpheus/carservice/controller/ClientController.java
@@ -9,9 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +40,7 @@ public class ClientController {
         return ResponseEntity.ok(clientService.createClient(clientDTO));
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<ClientDTO> updateClient(@PathVariable Long id,
                                                   @Validated(ValidationGroups.OnUpdate.class) @RequestBody ClientDTO clientDTO) {
         return ResponseEntity.ok(clientService.updateClient(id, clientDTO));
